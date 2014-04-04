b2c48f431661a914eee41cbcfdb0fa0f3eb32ce8
AnAlien <fadbz08@gmail.com>
2014-04-04 07:37:41 -0400

    Changed project name to Login and completed form

--- Login/src/com/fahad/beans/User.java
+++ Login/src/com/fahad/beans/User.java
@@ -1,6 +1,12 @@
 package com.fahad.beans;
 
 public class User {
+
+//	public static void main(String args[]) {
+//		User us = new User("fadbz08@gmail.com", "pass123456");
+//		System.out.println(us.validate());
+//	}
+
 	String email = "Enter Email";
 	String password = "Enter Pass";
 	String errorMessage = "";
@@ -36,33 +42,36 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	public boolean validate(){
-		
-		if(email != null){
+
+	public boolean validate() {
+
+		if (email == null) {
 			errorMessage = "You must enter an email";
+			System.out.println("email is null");
 			return false;
 		}
-		
-		if(password != null){
+
+		if (password == null) {
+			System.out.println("pass is null");
 			errorMessage = "You must enter a password";
 			return false;
 		}
-		
-		if(!email.matches("\\w+@\\w+\\.\\w+")){
+
+		if (!email.matches("\\w+@\\w+\\.\\w+")) {
 			errorMessage = "Invalid email address!";
 			return false;
 		}
-		
-		if(password.length() < 8){
+
+		if (password.length() < 8) {
+			System.out.println("less than 8");
 			errorMessage = "Password must be at least 8 characters long.";
 			return false;
 		}
-		if(password.matches("\\w*+\\s+\\w*")){
+		if (password.matches("\\w*+\\s+\\w*")) {
 			errorMessage = "Password cannot contain space.";
 			return false;
 		}
-	
+		System.out.println("Should be true");
 		return true;
 	}
 }
